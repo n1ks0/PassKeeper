6376d4d7a4588c9027e76fba2d655b2dd84ad0fc
VitalySubi <56627654+VitalySubi@users.noreply.github.com>
2025-01-09 23:16:02 +0800

    Тестовый коммит (#1)

--- PassKeeper.Core/Entities/User.cs
+++ PassKeeper.Core/Entities/User.cs
@@ -12,6 +12,9 @@ public class User : IUser
         Role = role;
     }
 
+    /// <summary>
+    /// ID
+    /// </summary>
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; }
     public string Email { get; set; }
