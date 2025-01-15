cc64f0d408f80decedfa697add411e61c8499d88
GT-SD <83197208+GT-SD@users.noreply.github.com>
2025-01-15 13:18:13 +0800

    First commit (#2)

--- PassKeeper.Core/Entities/User.cs
+++ PassKeeper.Core/Entities/User.cs
@@ -1,7 +1,9 @@
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PassKeeper.Core.Entities;
-
+/// <summary>
+/// Пользователь
+/// </summary>
 public class User : IUser
 {
     public User(string name, string email, string password, UserRole role)
@@ -16,9 +18,21 @@ public class User : IUser
     /// ID
     /// </summary>
     public Guid Id { get; set; } = Guid.NewGuid();
+    /// <summary>
+    /// Имя
+    /// </summary>
     public string Name { get; set; }
+    /// <summary>
+    /// Электронная почта
+    /// </summary>
     public string Email { get; set; }
+    /// <summary>
+    /// Пароль
+    /// </summary>
     public string Password { get; set; }
+    /// <summary>
+    /// Роль
+    /// </summary>
     public UserRole Role { get; set; }
     public bool IsVerified { get; set; }
     public ICollection<Secret> Secrets { get; set; } = new List<Secret>();
